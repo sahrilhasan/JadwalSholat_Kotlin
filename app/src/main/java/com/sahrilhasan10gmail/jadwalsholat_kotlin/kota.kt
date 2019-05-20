f43d1a2037645dd366c776ddab1b40c21136112d
Sahril <sahrilhasan10@gmail.com>
2019-05-20 11:23:56 +0700

    Initial commit

---
+++ app/src/main/java/com/sahrilhasan10gmail/jadwalsholat_kotlin/kota.kt
@@ -0,0 +1,10 @@
+package com.sahrilhasan10gmail.jadwalsholat_kotlin
+
+class Kota {
+    var id: Int? = null
+    var nama: String? = null
+
+    override fun toString(): String {
+        return nama.toString()
+    }
+}
